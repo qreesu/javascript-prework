37eda49c1cd6fe4683298b378f2f72e9a9ff34f6
Krzysztof Tylka <krzysztof.tylka@ttpsc.pl>
2019-12-14 15:37:36 +0100

    Update .gitignore. Add script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
-let computerMove = 'kamień'';
-let playerMove = 'papier';
+let roundNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + 'Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + playerMove + 'Jeśli Twój ruch to kamień, to przegrywasz!');
+let computerMove;
+if (roundNumber === 1) {
+  computerMove = 'kamień';
+} else if (roundNumber === 2) {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyce';
+}
+
+let playerInput = prompt('Wybierz swój ruch!\n1: kamień\n2: papier\n3: nożyce.');
+
+let playerMove;
+if (playerInput === '1') {
+  playerMove = 'kamień';
+} else if (playerInput === '2') {
+  playerMove = 'papier';
+} else if (playerInput === '3') {
+  playerMove = 'nożyce';
+} else {
+  console.log('nieprawidłowy wybór! - wprowadź liczbę z zakresu od 1 do 3')
+}
+
+if (playerInput < 4 && playerInput !== '') {
+  if (playerInput == roundNumber) {
+    console.log('remis');
+  } else if ((playerInput === '1' && roundNumber === 3) || (playerInput === '2' && roundNumber === 1) || (playerInput === '3' && roundNumber === 2)) {
+    console.log('wygrałeś rundę :) brawo!!')
+  } else {
+    console.log('komputer wygrał rundę :(')
+  }
+  console.log('Ty wybrałeś: ' + playerMove);
+  console.log('Komp wybrał: ' + computerMove);
+}
