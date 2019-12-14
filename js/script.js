b56a6b7038f755fb7796b053ed1636c89dda852e
Krzysztof Tylka <krzysztof.tylka@ttpsc.pl>
2019-12-14 15:48:52 +0100

    Update of implementation

--- js/script.js
+++ js/script.js
@@ -2,34 +2,36 @@ let roundNumber = Math.floor(Math.random() * 3 + 1);
 
 let computerMove;
 if (roundNumber === 1) {
-  computerMove = 'kamień';
+    computerMove = 'kamień';
 } else if (roundNumber === 2) {
-  computerMove = 'papier';
+    computerMove = 'papier';
 } else {
-  computerMove = 'nożyce';
+    computerMove = 'nożyce';
 }
 
 let playerInput = prompt('Wybierz swój ruch!\n1: kamień\n2: papier\n3: nożyce.');
 
 let playerMove;
 if (playerInput === '1') {
-  playerMove = 'kamień';
+    playerMove = 'kamień';
 } else if (playerInput === '2') {
-  playerMove = 'papier';
+    playerMove = 'papier';
 } else if (playerInput === '3') {
-  playerMove = 'nożyce';
+    playerMove = 'nożyce';
+} else if (playerInput === null) {
+    console.log('Może jednak zagrasz ze mną...?')
 } else {
-  console.log('nieprawidłowy wybór! - wprowadź liczbę z zakresu od 1 do 3')
+    console.log('nieprawidłowy wybór! - wprowadź liczbę z zakresu od 1 do 3')
 }
 
-if (playerInput < 4 && playerInput !== '') {
-  if (playerInput == roundNumber) {
-    console.log('remis');
-  } else if ((playerInput === '1' && roundNumber === 3) || (playerInput === '2' && roundNumber === 1) || (playerInput === '3' && roundNumber === 2)) {
-    console.log('wygrałeś rundę :) brawo!!')
-  } else {
-    console.log('komputer wygrał rundę :(')
-  }
-  console.log('Ty wybrałeś: ' + playerMove);
-  console.log('Komp wybrał: ' + computerMove);
+if (playerInput < 4 && playerInput !== '' && playerInput != null) {
+    if (playerInput == roundNumber) {
+        console.log('remis');
+    } else if ((playerInput === '1' && roundNumber === 3) || (playerInput === '2' && roundNumber === 1) || (playerInput === '3' && roundNumber === 2)) {
+        console.log('wygrałeś rundę :) brawo!!')
+    } else {
+        console.log('komputer wygrał rundę :(')
+    }
+    console.log('Ty wybrałeś: ' + playerMove);
+    console.log('Komp wybrał: ' + computerMove);
 }
