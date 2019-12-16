6812f3cddd5057241f5805a3ab6e3b5c5ffafb52
Krzysztof Tylka <krzysztof.tylka@ttpsc.pl>
2019-12-16 20:42:39 +0100

    Add listeners to buttons - 5.6 finished

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,40 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+function playGame(playerInput) {
+	clearMessages();
+	let randomNumber = Math.floor(Math.random() * 3 + 1);
+	let computerMove = getMoveName(randomNumber);
+	let playerMove = getMoveName(playerInput);
+	displayResult(computerMove, playerMove);
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+function getMoveName(moveId) {
+	if (moveId == 1) {
+		return 'kamień';
+	} else if (moveId == 2) {
+		return 'papier';
+	} else if (moveId == 3) {
+		return 'nożyce';
+	}
+}
+
+function displayResult(computerMove, playerMove) {
+	if (playerMove === computerMove) {
+		printMessage('Ty i komputer wybraliście ' + computerMove.toUpperCase() + '. Zatem jest remis w tej rundzie')
+	} else {
+		printMessage('Komputer wybrał ' + computerMove.toUpperCase() + ', a Ty wybrałaś/eś ' + playerMove.toUpperCase());
+		if ((playerMove === 'kamień' && computerMove === 'nożyce') || (playerMove === 'papier' && computerMove === 'kamień') || (playerMove === 'nożyce' && computerMove === 'papier')) {
+			printMessage('Wygrywasz rundę :) Brawo!!');
+		} else {
+			printMessage('Niestety, tym razem wygrywa komputer :(');
+		}
+	}
+}
+
+function printMessage(msg) {
+    let div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages() {
+    document.getElementById('messages').innerHTML = '';
 }
--- js/script.js
+++ js/script.js
@@ -1,35 +1,9 @@
-function getMoveName(moveId) {
-    if (moveId == 1) {
-        return 'kamień';
-    } else if (moveId == 2) {
-        return 'papier';
-    } else if (moveId == 3) {
-        return 'nożyce';
-    }
-}
-
-function displayResult(computerMove, playerMove) {
-    if (playerInput == randomNumber) {
-        printMessage('Ty i komputer wybraliście ' + computerMove.toUpperCase() + '. Zatem jest remis w tej rundzie')
-    } else {
-        printMessage('Komputer wybrał ' + computerMove.toUpperCase() + ', a Ty wybrałaś/eś ' + playerMove.toUpperCase());
-        if ((playerMove === 'kamień' && computerMove === 'nożyce') || (playerMove === 'papier' && computerMove === 'kamień') || (playerMove === 'nożyce' && computerMove === 'papier')) {
-            printMessage('Wygrywasz rundę :) Brawo!!');
-        } else {
-            printMessage('Niestety, tym razem wygrywa komputer :(');
-        }
-    }
-}
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randomNumber);
-let playerInput = prompt('Wybierz swój ruch:\n1 -> kamień\n2 -> papier\n3 -> nożyce');
-let playerMove;
-if (playerInput === null) {
-    printMessage('Może jednak zagrasz ze mną...?\nWciśnij F5')
-} else if ((playerInput < 1 || playerInput > 3) || playerInput === '') {
-    printMessage('Nieprawidłowy wybór. Wybierz liczbę z zakresu od 1 do 3');
-} else {
-    playerMove = getMoveName(playerInput);
-    displayResult(computerMove, playerMove);
-}
+document.getElementById('play-rock').addEventListener('click', function () {
+    playGame(1)
+})
+document.getElementById('play-paper').addEventListener('click', function () {
+    playGame(2)
+})
+document.getElementById('play-scissors').addEventListener('click', function () {
+    playGame(3)
+})
